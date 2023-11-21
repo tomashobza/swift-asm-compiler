4fa246f343f7dd7fd30701637e14fbf4f62232f8
Tomáš Hobza <thomashobza@ids.company>
2023-11-21 23:57:38 +0100

    update: test main

--- tests/test.c
+++ tests/test.c
@@ -15,38 +15,70 @@
 
 int main(void)
 {
+    scanner_init();
 
-    // psa_return_type psa_ret = parse_expression();
-    //
-    // printf_magenta("\n\n==== PSA RETURN ====\n");
-    // printf("Is ok: ");
-    // if (psa_ret.is_ok)
-    //{
-    //    printf_green("true\n");
-    //}
-    // else
-    //{
-    //    printf_red("false\n");
-    //}
-    //
-    // printf("Type: ");
-    // print_expression_type(psa_ret.type);
-    //
-    // printf("End token: ");
-    // print_token_type(psa_ret.end_token);
-    //
-    // printf("Can be nil: ");
-    // if (psa_ret.canBeNil)
-    //{
-    //    printf_green("true\n");
-    //}
-    // else
-    //{
-    //    printf_red("false\n");
-    //}
-    // printf_magenta("====================\n");
+    symtable_stack *st_stack = symtable_stack_init();
+    symtable_stack_push(st_stack, symtable_init());
 
-    parser_main();
+    ParamData params[2] = {
+        (ParamData){
+            .name = "_",
+            .id = "a",
+            .type = "int",
+        },
+        (ParamData){
+            .name = "b",
+            .id = "b",
+            .type = TYPE_BOOL,
+        },
+    };
+
+    symtable_item new_item = (symtable_item){
+        .id = "foo",
+        .type = FUNCTION,
+        .data = {
+            .func_data = &(FunctionData){
+                .return_type = "int",
+                .params = params,
+                .params_count = 2,
+                .capacity = 0,
+            },
+        },
+    };
+
+    symtable_add(new_item, symtable_stack_top(st_stack));
+
+    psa_return_type psa_ret = parse_expression(st_stack);
+
+    printf_magenta("\n\n==== PSA RETURN ====\n");
+    printf("Is ok: ");
+    if (psa_ret.is_ok)
+    {
+        printf_green("true\n");
+    }
+    else
+    {
+        printf_red("false\n");
+    }
+
+    printf("Type: ");
+    print_expression_type(psa_ret.type);
+
+    printf("End token: ");
+    print_token_type(psa_ret.end_token);
+
+    printf("Can be nil: ");
+    if (psa_ret.canBeNil)
+    {
+        printf_green("true\n");
+    }
+    else
+    {
+        printf_red("false\n");
+    }
+    printf_magenta("====================\n");
+
+    // parser_main();
 
     return EXIT_SUCCESS;
 }
