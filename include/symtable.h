ca5898a12fe84ee19d502844a6f1465bde675b01
Jakub Vsetecka <jakub01vsetecka@seznam.cz>
2023-11-21 18:42:22 +0100

    changed item type from char* to Expression_type. moved Expression_type definition from psa.h to symtable.h

--- include/symtable.h
+++ include/symtable.h
@@ -17,9 +17,23 @@
 #include <stdbool.h>
 #include "stack.h"
 
+/**
+ * @brief Enum for the types of the expression.
+ */
+typedef enum
+{
+    TYPE_EMPTY = -2,   // empty expression
+    TYPE_INVALID = -1, // invalid expression
+    TYPE_INT = 0,      // int
+    TYPE_DOUBLE = 1,   // double
+    TYPE_STRING = 2,   // string
+    TYPE_BOOL = 3,     // bool
+    TYPE_NIL = 4,      // nil
+} Expression_type;
+
 typedef struct
 {
-    char *type;
+    Expression_type type;
     bool is_const;
     bool is_initialized;
 } VariableData;
@@ -28,12 +42,12 @@ typedef struct
 {
     char *name;
     char *id;
-    char *type;
+    Expression_type type;
 } ParamData;
 
 typedef struct
 {
-    char *return_type;
+    Expression_type return_type;
     bool is_defined;
     ParamData *params;
     int params_count;
--- src/semantic.c
+++ src/semantic.c
@@ -67,13 +67,13 @@ void reset_param()
 {
     new_param.name = NULL;
     new_param.id = NULL;
-    new_param.type = NULL;
+    new_param.type = TYPE_EMPTY;
 }
 
 void reset_var()
 {
     varItem->id = NULL;
-    varItem->data.var_data->type = NULL;
+    varItem->data.var_data->type = TYPE_EMPTY;
     varItem->data.var_data->is_const = false;
     varItem->data.var_data->is_initialized = false;
 }
@@ -81,7 +81,7 @@ void reset_var()
 void reset_func()
 {
     funcItem->id = NULL;
-    funcItem->data.func_data->return_type = "Void";
+    funcItem->data.func_data->return_type = TYPE_EMPTY;
     funcItem->data.func_data->is_defined = false;
     free(funcItem->data.func_data->params);
     funcItem->data.func_data->params = NULL;
@@ -101,14 +101,39 @@ void semantic_destroy()
     // stack_free(myStack);
 }
 
+Expression_type get_expression_type(Token **token)
+{
+    switch ((*token)->type)
+    {
+    case TOKEN_INT:
+        return TYPE_INT;
+        break;
+    case TOKEN_DOUBLE:
+        return TYPE_DOUBLE;
+        break;
+    case TOKEN_STRING:
+        return TYPE_STRING;
+        break;
+    case TOKEN_BOOL:
+        return TYPE_BOOL;
+        break;
+    case TOKEN_NIL:
+        return TYPE_NIL;
+        break;
+    default:
+        return TYPE_INVALID;
+        break;
+    }
+}
+
 void print_items()
 {
-    printf(MAGENTA "FUNCTION: %s, return type: %s, is defined: %d" RESET "\n", funcItem->id, funcItem->data.func_data->return_type, funcItem->data.func_data->is_defined);
+    printf(MAGENTA "FUNCTION: %s, return type: %d, is defined: %d" RESET "\n", funcItem->id, funcItem->data.func_data->return_type, funcItem->data.func_data->is_defined);
     for (int i = 0; i < funcItem->data.func_data->params_count; i++)
     {
-        printf(MAGENTA "PARAM: %s, id: %s, type: %s" RESET "\n", funcItem->data.func_data->params[i].name, funcItem->data.func_data->params[i].id, funcItem->data.func_data->params[i].type);
+        printf(MAGENTA "PARAM: %s, id: %s, type: %d" RESET "\n", funcItem->data.func_data->params[i].name, funcItem->data.func_data->params[i].id, funcItem->data.func_data->params[i].type);
     }
-    printf(BLUE "VARIABLE: %s, type: %s, is const: %d" RESET "\n", varItem->id, varItem->data.var_data->type, varItem->data.var_data->is_const);
+    printf(BLUE "VARIABLE: %s, type: %d, is const: %d" RESET "\n", varItem->id, varItem->data.var_data->type, varItem->data.var_data->is_const);
 }
 
 int check_semantic(Token **token, Sem_rule sem_rule)
@@ -128,7 +153,7 @@ int check_semantic(Token **token, Sem_rule sem_rule)
         varItem->id = (*token)->token_value;
         break;
     case VAR_TYPE:
-        varItem->data.var_data->type = (*token)->token_value;
+        varItem->data.var_data->type = get_expression_type(token);
         // todo semantic checks
         symtable_add(*varItem, symtable_stack_top(stack));
         break;
@@ -162,12 +187,12 @@ int check_semantic(Token **token, Sem_rule sem_rule)
         new_param.id = (*token)->token_value;
         break;
     case P_TYPE:
-        new_param.type = (*token)->token_value;
+        new_param.type = get_expression_type(token);
         add_param(funcItem->data.func_data, new_param);
         reset_param();
         break;
     case R_TYPE:
-        funcItem->data.func_data->return_type = (*token)->token_value;
+        funcItem->data.func_data->return_type = get_expression_type(token);
         break;
     case FUNC_HEADER_DONE:
         funcItem->data.func_data->is_defined = true;
