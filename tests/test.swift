4231b3445d665c7e2feb79dbea33351128de14aa
Jakub Vsetecka <jakub01vsetecka@seznam.cz>
2023-11-28 11:29:39 +0100

    tests

--- tests/test.swift
+++ tests/test.swift
@@ -1,8 +1,2 @@
-func concat(_ x : String, with y : String) -> String {
-var x = x + y
-return x
-}
-let a = "ahoj "
-var ct : String
-ct = concat(a, with: "svete")
-write(ct, a)
+// NO_ERR
+let x = 2
