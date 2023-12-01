0c79ef6c0ff3464d225ed4264d723220c6cc5c9b
Tomáš Hobza <thomashobza@ids.company>
2023-12-01 15:19:43 +0100

    add: generator

---
+++ generator.c
@@ -0,0 +1,10 @@
+/**
+ * @file generator.c
+ * @author Tomáš Hobza (xhobza03@vutbr.cz), Jakub Všetečka (xvsete00@vutbr.cz)
+ * @brief Generator of IFJcode20.
+ * @version 0.1
+ * @date 2023-11-24
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
---
+++ generator.h
@@ -0,0 +1,10 @@
+/**
+ * @file generator.h
+ * @author Tomáš Hobza (xhobza03@vutbr.cz), Jakub Všetečka (xvsete00@vutbr.cz)
+ * @brief Header file for the ifjcode2023 code generator.
+ * @version 0.1
+ * @date 2023-11-24
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
