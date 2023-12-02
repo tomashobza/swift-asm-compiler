a733b5d8a159a60c174f517a193e0b5fa9fe71b5
Jakub Vsetecka <jakub01vsetecka@seznam.cz>
2023-12-02 16:14:49 +0100

    test

--- tests/test.swift
+++ tests/test.swift
@@ -1,26 +1,57 @@
-// NO_ERR
-func 
+var x :Bool
+x = 5 + 5
+x = 5 + 5
+x = 5 + 5
+func foo (  )  ->Int {
+var x :Int
+var x :Int
+var x :Int?
 
+while 5 + 5 {
 
+while 5 + 5 {
 
+while 5 + 5 {
+var x :Double?
+var x :Double?
+var x :Bool
+var x :Bool
+var x :String
 
+if 5 + 5 {
 
+if 5 + 5 {
 
+if let x {
+var x :Int?
+var x :String?
 
+if 5 + 5 {
 
-test
+if 5 + 5 {
 
+if 5 + 5 {
 
+if 5 + 5 {
 
+if 5 + 5 {
 
-
-()
-
-
-
-
-
-
-
-
-{}
+if 5 + 5 {
+var x :Double
+var x :Double
+x = 5 + 5
+x = 5 + 5
+x = 5 + 5
+ }
+ }
+ }
+ }
+ }
+ }
+ }
+ }
+ }
+}
+}
+}
+}
