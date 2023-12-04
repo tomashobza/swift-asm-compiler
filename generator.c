52b0a44046210454bd8fce3ccfada6821c7d7df8
Tomáš Hobza <thomashobza@ids.company>
2023-12-04 21:45:57 +0100

    fix: ifs

--- generator.c
+++ generator.c
@@ -448,8 +448,10 @@ void generate_else()
     char *else_lbl = malloc(sizeof(char) * 10);
     sprintf(else_lbl, "else%d_%d", if_counter, elif_counter);
 
+    elif_counter++;
+
     char *endif_lbl = malloc(sizeof(char) * 10);
-    sprintf(endif_lbl, "endif%d", if_counter);
+    sprintf(endif_lbl, "else%d_%d", if_counter, elif_counter);
 
     fprintf(out_code_file, "# if%d else\n", if_counter);
     generate_instruction(JUMP, label(endif_lbl));
@@ -461,7 +463,6 @@ void generate_else()
     free(else_lbl);
     free(endif_lbl);
 
-    elif_counter++;
     if_counter++;
 }
 
@@ -472,7 +473,7 @@ void generate_if_end()
     fprintf(out_code_file, "# if%d end\n", if_counter);
 
     char *endif_lbl = malloc(sizeof(char) * 10);
-    sprintf(endif_lbl, "endif%d", if_counter);
+    sprintf(endif_lbl, "else%d_%d", if_counter, elif_counter);
     generate_instruction(LABEL, label(endif_lbl));
 
     fprintf(out_code_file, "\n");
