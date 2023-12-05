0e5c391ef3f2369b1e5bd70112b5cde37a26f545
Tomáš Hobza <thomashobza@ids.company>
2023-12-05 11:23:59 +0100

    add: comment

--- io.c
+++ io.c
@@ -75,6 +75,8 @@ PSA_Token readNextToken(PSA_Token_stack *s, char *next_token_error, int *num_of_
             int param_count = 0;
             b = parseFunctionCall(s, b, &param_count);
 
+            // YEET - tady se vola a funkce
+
             if (b.expr_type != TYPE_INVALID && b.type != TOKEN_EOF)
             {
                 if (isBuiltInFunction(convertPSATokenToToken(b)))
