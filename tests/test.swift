aa4fef3489f31f6d4c70f4872d0d3569f00ae9bc
Tomáš Hobza <xhobza03@vutbr.cz>
2023-12-06 00:26:33 +0100

    add: test

--- tests/test.swift
+++ tests/test.swift
@@ -1,23 +1,4 @@
-func boo () -> Bool {
-    if true {
-        return true
-    } else {
-        return false
-    }
-}
-
-func foo () -> Bool {
-    if boo() {
-        return true
-    } else if boo() {
-        if boo() {
-            return true
-        } else {
-            if boo() {
-                return true
-            }
-        }
-    } else {
-        return false
-    }
-}
+var x = false
+  if !x {
+    write("Not false")
+  }
