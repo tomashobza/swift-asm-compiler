5c0683d2d57ae06fdaaf38a0cbb633a78f9149f8
Tomáš Hobza <thomashobza@ids.company>
2023-12-06 23:42:13 +0100

    add: archive

--- odevzdavani/archive.sh
+++ odevzdavani/archive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname "$0")/../"
-zip -r odevzdavani/xhobza03.zip *.c *.h Makefile documentation ifjcompiler README.md rozdeleni
+zip -r odevzdavani/xhobza03.zip *.c *.h Makefile dokumentace.pdf ifjcompiler README.md rozdeleni rozsireni
