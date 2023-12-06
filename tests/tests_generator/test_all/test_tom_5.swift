dc6789f860df2c14bbe4d65d4e7255d632dd8057
Tomáš Hobza <xhobza03@vutbr.cz>
2023-12-06 14:37:02 +0100

    add: more tests

---
+++ tests/tests_generator/test_all/test_tom_5.swift
@@ -0,0 +1,13 @@
+func foo(a _ : Int, _ b: Double) -> String {
+    if (b >= 0.0) {
+        return "je pozitivne"
+    } 
+
+    return "je zaporne"
+}
+
+let y : Int? = 5
+let x = Int2Double(y!)
+
+write(foo(a: 69, x) + "\n")
+write(foo(a: 69, 0-x))
